94b7be0e0ecf4fc965a988d496095081bdd74376
charan-ivesk <charan.annadurai@hs-offenburg.de>
2024-05-21 15:39:01 +0200

    works finally

--- asset-transfer-basic/chaincode-javascript/lib/assetTransfer.js
+++ asset-transfer-basic/chaincode-javascript/lib/assetTransfer.js
@@ -13,154 +13,243 @@ const { Contract } = require('fabric-contract-api');
 
 class AssetTransfer extends Contract {
 
-    async InitLedger(ctx) {
-        const assets = [
-            {
-                ID: 'asset1',
-                Color: 'blue',
-                Size: 5,
-                Owner: 'Tomoko',
-                AppraisedValue: 300,
-            },
-            {
-                ID: 'asset2',
-                Color: 'red',
-                Size: 5,
-                Owner: 'Brad',
-                AppraisedValue: 400,
-            },
-            {
-                ID: 'asset3',
-                Color: 'green',
-                Size: 10,
-                Owner: 'Jin Soo',
-                AppraisedValue: 500,
-            },
-            {
-                ID: 'asset4',
-                Color: 'yellow',
-                Size: 10,
-                Owner: 'Max',
-                AppraisedValue: 600,
-            },
-            {
-                ID: 'asset5',
-                Color: 'black',
-                Size: 15,
-                Owner: 'Adriana',
-                AppraisedValue: 700,
-            },
-            {
-                ID: 'asset6',
-                Color: 'white',
-                Size: 15,
-                Owner: 'Michel',
-                AppraisedValue: 800,
-            },
-        ];
+    async initLedger(ctx){
+        await ctx.stub.putState("test","hello world");
+        return "success";
+    }
 
-        for (const asset of assets) {
-            asset.docType = 'asset';
-            // example of how to write to world state deterministically
-            // use convetion of alphabetic order
-            // we insert data in alphabetic order using 'json-stringify-deterministic' and 'sort-keys-recursive'
-            // when retrieving data, in any lang, the order of data will be the same and consequently also the corresonding hash
-            await ctx.stub.putState(asset.ID, Buffer.from(stringify(sortKeysRecursive(asset))));
+    async writeData(ctx, key, value){
+        await ctx.stub.putState(key,value);
+        return value;
+    }
+
+    async readData(ctx, key){
+        var response = await ctx.stub.getState(key);
+        return response.toString();
+    }    
+
+    async queryByID(ctx, ID){
+       let queryString ={}
+       queryString.selector={"_id":ID}
+       let iterator = await ctx.stub.getQueryResult(JSON.stringify(queryString))
+       let result = await this.getIteratorData(iterator)
+       return result
+    }    
+
+    async queryValidFB(ctx, g_type){
+        let queryString ={}
+        queryString.selector={
+            "_id": {
+                "$regex": "FB_"
+             },
+            "grain":g_type,
+            "status":"RECEIVED"
         }
-    }
+        let iterator = await ctx.stub.getQueryResult(JSON.stringify(queryString))
+        let result = await this.getIteratorData(iterator)
+        return result
+     } 
 
-    // CreateAsset issues a new asset to the world state with given details.
-    async CreateAsset(ctx, id, color, size, owner, appraisedValue) {
-        const exists = await this.AssetExists(ctx, id);
-        if (exists) {
-            throw new Error(`The asset ${id} already exists`);
+    async queryValidFB(ctx, g_type){
+        let queryString ={}
+        queryString.selector={
+            "_id": {
+                "$regex": "FB_"
+             },
+            "grain":g_type,
+            "status":"RECEIVED"
         }
-
-        const asset = {
-            ID: id,
-            Color: color,
-            Size: size,
-            Owner: owner,
-            AppraisedValue: appraisedValue,
-        };
-        // we insert data in alphabetic order using 'json-stringify-deterministic' and 'sort-keys-recursive'
-        await ctx.stub.putState(id, Buffer.from(stringify(sortKeysRecursive(asset))));
-        return JSON.stringify(asset);
-    }
-
-    // ReadAsset returns the asset stored in the world state with given id.
-    async ReadAsset(ctx, id) {
-        const assetJSON = await ctx.stub.getState(id); // get the asset from chaincode state
-        if (!assetJSON || assetJSON.length === 0) {
-            throw new Error(`The asset ${id} does not exist`);
+        let iterator = await ctx.stub.getQueryResult(JSON.stringify(queryString))
+        let result = await this.getIteratorData(iterator)
+        return result
+    } 
+     
+    async queryValidZFB(ctx){
+        let queryString ={}
+        queryString.selector={
+            "_id": {
+                "$regex": "ZF_"
+             },
+            "status":"RECEIVED"
         }
-        return assetJSON.toString();
-    }
-
-    // UpdateAsset updates an existing asset in the world state with provided parameters.
-    async UpdateAsset(ctx, id, color, size, owner, appraisedValue) {
-        const exists = await this.AssetExists(ctx, id);
-        if (!exists) {
-            throw new Error(`The asset ${id} does not exist`);
+        let iterator = await ctx.stub.getQueryResult(JSON.stringify(queryString))
+        let result = await this.getIteratorData(iterator)
+        return result
+     }
+    
+    async queryAvailableZFB(ctx){
+        let queryString ={}
+        queryString.selector={
+            "_id": {
+                "$regex": "ZF_"
+             },
+            "status":"READY"
         }
+        let iterator = await ctx.stub.getQueryResult(JSON.stringify(queryString))
+        let result = await this.getIteratorData(iterator)
+        return result
+     }    
 
-        // overwriting original asset with new asset
-        const updatedAsset = {
-            ID: id,
-            Color: color,
-            Size: size,
-            Owner: owner,
-            AppraisedValue: appraisedValue,
-        };
-        // we insert data in alphabetic order using 'json-stringify-deterministic' and 'sort-keys-recursive'
-        return ctx.stub.putState(id, Buffer.from(stringify(sortKeysRecursive(updatedAsset))));
-    }
-
-    // DeleteAsset deletes an given asset from the world state.
-    async DeleteAsset(ctx, id) {
-        const exists = await this.AssetExists(ctx, id);
-        if (!exists) {
-            throw new Error(`The asset ${id} does not exist`);
+    async queryAvailableDrivers(ctx){
+        let queryString ={}
+        queryString.selector={
+            "_id": {
+                "$regex": "DR_"
+             },
+            "status":"AVAILABLE"
         }
-        return ctx.stub.deleteState(id);
-    }
+        let iterator = await ctx.stub.getQueryResult(JSON.stringify(queryString))
+        let result = await this.getIteratorData(iterator)
+        return result
+     }    
 
-    // AssetExists returns true when asset with given ID exists in world state.
-    async AssetExists(ctx, id) {
-        const assetJSON = await ctx.stub.getState(id);
-        return assetJSON && assetJSON.length > 0;
-    }
+     async pendingPur(ctx){
+        let queryString ={}
+        queryString.selector={
+            "_id": {
+                "$regex": "PU_"
+             },
+            "status":"CREATED"
+        }
+        let iterator = await ctx.stub.getQueryResult(JSON.stringify(queryString))
+        let result = await this.getIteratorData(iterator)
+        return result
+     }    
 
-    // TransferAsset updates the owner field of asset with given id in the world state.
-    async TransferAsset(ctx, id, newOwner) {
-        const assetString = await this.ReadAsset(ctx, id);
-        const asset = JSON.parse(assetString);
-        const oldOwner = asset.Owner;
-        asset.Owner = newOwner;
-        // we insert data in alphabetic order using 'json-stringify-deterministic' and 'sort-keys-recursive'
-        await ctx.stub.putState(id, Buffer.from(stringify(sortKeysRecursive(asset))));
-        return oldOwner;
-    }
+     async queryPendingTranport(ctx){
+        let queryString ={}
+        queryString.selector={
+            "_id": {
+                "$regex": "TR_"
+             },
+            "status":"INITIATED"
+        }
+        let iterator = await ctx.stub.getQueryResult(JSON.stringify(queryString))
+        let result = await this.getIteratorData(iterator)
+        return result
+     }    
 
-    // GetAllAssets returns all assets found in the world state.
-    async GetAllAssets(ctx) {
-        const allResults = [];
-        // range query with empty string for startKey and endKey does an open-ended query of all assets in the chaincode namespace.
-        const iterator = await ctx.stub.getStateByRange('', '');
-        let result = await iterator.next();
-        while (!result.done) {
-            const strValue = Buffer.from(result.value.value.toString()).toString('utf8');
-            let record;
-            try {
-                record = JSON.parse(strValue);
-            } catch (err) {
-                console.log(err);
-                record = strValue;
+
+    async queryAllPurchases(ctx){
+        let queryString ={}
+        queryString.selector={      "_id": {
+            "$regex": "PU_"
+         }}
+        let iterator = await ctx.stub.getQueryResult(JSON.stringify(queryString))
+        let result = await this.getIteratorData(iterator)
+  
+        return result
+     }    
+
+     async queryAllSuppliers(ctx){
+        let queryString ={}
+        queryString.selector={      "_id": {
+            "$regex": "SP_"
+         }}
+        let iterator = await ctx.stub.getQueryResult(JSON.stringify(queryString))
+        let result = await this.getIteratorData(iterator)
+  
+        return result
+     }    
+
+     async queryAllQR(ctx){
+        let queryString ={}
+        queryString.selector={      "_id": {
+            "$regex": "QR_"
+         }}
+        let iterator = await ctx.stub.getQueryResult(JSON.stringify(queryString))
+        let result = await this.getIteratorData(iterator)
+  
+        return result
+     }    
+
+     async queryAllEmployees(ctx){
+        let queryString ={}
+        queryString.selector={      "_id": {
+            "$regex": "EM_"
+         }}
+        let iterator = await ctx.stub.getQueryResult(JSON.stringify(queryString))
+        let result = await this.getIteratorData(iterator)
+  
+        return result
+     }    
+
+
+     async queryAllZFB(ctx){
+        let queryString ={}
+        queryString.selector={      "_id": {
+            "$regex": "ZF_"
+         }}
+        let iterator = await ctx.stub.getQueryResult(JSON.stringify(queryString))
+        let result = await this.getIteratorData(iterator)
+  
+        return result
+     }    
+
+     async queryAllTransports(ctx){
+        let queryString ={}
+        queryString.selector={      "_id": {
+            "$regex": "TR_"
+         }}
+        let iterator = await ctx.stub.getQueryResult(JSON.stringify(queryString))
+        let result = await this.getIteratorData(iterator)
+  
+        return result
+     }  
+
+     async queryAllDrivers(ctx){
+        let queryString ={}
+        queryString.selector={      "_id": {
+            "$regex": "DR_"
+         }}
+        let iterator = await ctx.stub.getQueryResult(JSON.stringify(queryString))
+        let result = await this.getIteratorData(iterator)
+  
+        return result
+     }    
+ 
+    async queryAllFarmerBags(ctx){
+        let queryString ={}
+        queryString.selector={      "_id": {
+            "$regex": "FB_"
+            }}
+        let iterator = await ctx.stub.getQueryResult(JSON.stringify(queryString))
+        let result = await this.getIteratorData(iterator)
+
+        return result
+        }  
+
+    async queryAllBatches(ctx){
+        let queryString ={}
+        queryString.selector={      "_id": {
+            "$regex": "BA_"
+            }}
+        let iterator = await ctx.stub.getQueryResult(JSON.stringify(queryString))
+        let result = await this.getIteratorData(iterator)
+
+        return result
+        }   
+
+    
+    async getIteratorData(iterator){
+        let outputArray = []
+
+        while(true){
+            let res=await iterator.next();
+            let jsonValue = {}
+            if(res.value && res.value.value.toString('utf-8')){
+                let str=res.value.key
+                str=str.slice(3,str.length)
+                jsonValue.id = str;
+                jsonValue.value = JSON.parse(res.value.value.toString('utf-8'));
+                outputArray.push(jsonValue)
             }
-            allResults.push(record);
-            result = await iterator.next();
+            
+            if(res.done){
+                await iterator.close();
+                return outputArray;
+            }
         }
-        return JSON.stringify(allResults);
     }
 }
 
